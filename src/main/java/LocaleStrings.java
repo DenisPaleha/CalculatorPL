51d8c410186c6a2d79db20e4d81e2bbf3951a284
DenisPaleha <portogas@gmail.com>
2023-10-05 16:25:54 +0200

    code cleanup

--- src/main/java/LocaleStrings.java
+++ src/main/java/LocaleStrings.java
@@ -1,10 +1,8 @@
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class LocaleStrings {
-
-    private Properties properties;
+    private final Properties properties;
 
     public LocaleStrings(Boolean isEnglish) {
         properties = new Properties();
@@ -26,21 +24,6 @@ public class LocaleStrings {
         }
     }
 
-    public String getString(String key) {
-        return properties.getProperty(key);
-    }
+    public String getString(String key){return properties.getProperty(key);}
 
-    public static void main(String[] args) {
-        boolean isEnglish = false;
-
-        LocaleStrings localeStrings = new LocaleStrings(isEnglish); // Убедитесь, что путь к файлу правильный
-        String hello1 = localeStrings.getString("hello_massage_one");
-        String hello2 = localeStrings.getString("hello_massage_two");
-        String roman = localeStrings.getString("roman_number");
-        String equal_decimal = localeStrings.getString("equal_decimal");
-        System.out.println(hello1);
-        System.out.println(hello2);
-        System.out.println(roman + "C " + equal_decimal + "100.00");
-
-    }
 }
